6e0045a3f6b8297cb5432d65a161a75e16174631
Vanaiyalini <kyalini2001@gmail.com>
2026-01-02 14:29:21 +0530

    change in vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/TaskTrail/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000', // Proxy API calls to backend
